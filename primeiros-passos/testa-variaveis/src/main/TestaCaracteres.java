72011a82c08b3cbc1e9782ad2c097527c73daf75
GAMELEIRA <gameleira270499@gmail.com>
2020-05-18 00:02:45 -0300

    UTILIZAR O CHAR E STRINGS

---
+++ primeiros-passos/testa-variaveis/src/main/TestaCaracteres.java
@@ -0,0 +1,26 @@
+package main;
+
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 17/05/2020
+ * HORA: 23:50
+ * FINALIDADE DO PROGRAMA: APRENDER A TRABALHAR COM CHAR E STRING
+ */
+
+public class TestaCaracteres {
+
+	public static void main(String[] args) {
+
+		char letra = 'a';
+		
+		System.out.println(letra);
+		
+		String palavra = "Gabriel Gameleira dos Santos";
+		
+		palavra += 1999;
+		
+		System.out.println(palavra);
+		
+	}
+
+}
