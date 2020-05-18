16a3282db3edf6371136bf4150d620534853bdc3
GAMELEIRA <gameleira270499@gmail.com>
2020-05-17 23:50:07 -0300

    Criado a classe testa conversao para trabalhar com casting

---
+++ primeiros-passos/testa-variaveis/src/main/TestaConversao.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.math.BigInteger;
+
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 17/05/2020
+ * HORA: 23:30
+ * FINALIDADE DO PROGRAMA: APRESENTAR UMA MENSAGEM DE BOAS VINDAS NO CONSOLEN
+ */
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		
+		double salario = 1270.50;
+		
+		int valor = (int) salario; //casting
+		
+		System.out.println(valor);
+		
+		long numeroGrande = 323233232323L;
+		
+		short valorPequeno = 2131;
+		
+		byte b = 127;
+		
+		
+	}
+
+}
---
+++ primeiros-passos/testa-variaveis/src/main/TestaPontoFlutuante.java
@@ -0,0 +1,30 @@
+package main;
+
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 17/05/2020
+ * HORA: 23:14
+ * FINALIDADE DO PROGRAMA: APRESENTAR UMA MENSAGEM DE BOAS VINDAS NO CONSOLEN
+ */
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double salario;
+		
+		salario = 1250.70;
+		
+		System.out.println("Meu salário é " + salario);
+		
+		double idade = 37;
+		
+		double divisao = 3.14 / 2;
+		
+		System.out.println(divisao);
+		
+		System.out.printf("%.2f", divisao);
+		
+	}
+
+}
